6c3a24f14116e6f3e74753718001db2073f86e0b
Alex Der <142178465+aldeau42@users.noreply.github.com>
2025-07-03 00:48:56 +0200

    Add files via upload

--- P2/provision-server.sh
+++ P2/provision-server.sh
@@ -6,7 +6,11 @@ apk upgrade
 apk add curl
 
 # Install K3s in server mode
-curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server --cluster-init --snapshotter=native --write-kubeconfig-mode 644" sh -
+curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server \
+  --cluster-init \
+  --snapshotter=native \
+  --write-kubeconfig-mode 644 \
+  --flannel-iface eth1" sh -
 
 # Wait for K3s to be ready
 echo "Waiting for K3s to start..."
@@ -14,16 +18,23 @@ while [ ! -f /var/lib/rancher/k3s/server/node-token ]; do
   sleep 2
 done
 
+NODE_TOKEN="/var/lib/rancher/k3s/server/node-token"
+YML="/etc/rancher/k3s/k3s.yaml"
+
 # Share the token with the worker node
-sudo cat /var/lib/rancher/k3s/server/node-token > /tmp/node-token
+mkdir -p /vagrant_shared
+cp ${NODE_TOKEN} /vagrant_shared/node-token
+cp ${YML} /vagrant_shared/k3s.yaml
 
 # Wait for kubeconfig to be available
-while [ ! -f /etc/rancher/k3s/k3s.yaml ]; do
-  sleep 2
+while [ ! -f ${YML} ]; do
+  sleep 5
 done
 
-# Configure kubectl
-mkdir -p $HOME/.kube
-cp /etc/rancher/k3s/k3s.yaml $HOME/.kube/config
-chown $USER:$USER $HOME/.kube/config
+# Copy kubeconfig for user 'vagrant'
+mkdir -p /home/vagrant/.kube
+cp /etc/rancher/k3s/k3s.yaml /home/vagrant/.kube/config
+chown vagrant:vagrant /home/vagrant/.kube/config
+
+
 
